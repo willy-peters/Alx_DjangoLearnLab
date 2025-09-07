1cc2176ebf9dabd3d42315ea3a2fe10c8a477b81
Willy Nguyamu <nguyamuwilly@gmail.com>
2025-09-07 22:30:07 +0300

    Fix query_sample file

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -1,11 +1,11 @@
 from relationship_app.models import Author, Book, Library, Librarian
 
 def run_queries():
-    # Query all books by a specific author
+    # Query all books by a specific author using filter
     author_name = "J.K. Rowling"
     try:
         author = Author.objects.get(name=author_name)
-        books_by_author = author.books.all()
+        books_by_author = Book.objects.filter(author=author)  # <--- filter()
         print(f"Books by {author_name}: {[book.title for book in books_by_author]}")
     except Author.DoesNotExist:
         print(f"No author found with name {author_name}")
@@ -21,7 +21,7 @@ def run_queries():
 
     # Retrieve the librarian for a library
     try:
-        librarian = library.librarian  # thanks to OneToOne relation
+        librarian = library.librarian  # OneToOne relation
         print(f"Librarian for {library_name}: {librarian.name}")
     except:
         print(f"No librarian assigned to {library_name}")

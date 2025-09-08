6f535089967b230df002c3d1be54fced6bc2b49e
Willy Nguyamu <nguyamuwilly@gmail.com>
2025-09-08 22:33:46 +0300

    change urls.py imports

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from .views import LibraryDetailView
+from .views import list_books
 
 urlpatterns = [
     # path("books/", views.list_books, name="list_books"),  # function-based view

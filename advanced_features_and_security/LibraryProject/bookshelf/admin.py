a8bdb589a1897596db1f6bd5c6fa2110b5e22871
Willy Nguyamu <nguyamuwilly@gmail.com>
2025-09-14 23:29:37 +0300

    Change admin.py

--- advanced_features_and_security/LibraryProject/bookshelf/admin.py
+++ advanced_features_and_security/LibraryProject/bookshelf/admin.py
@@ -1,10 +1,25 @@
 from django.contrib import admin
-from .models import Book
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser
 
-# Register your models here.
-class BookAdmin(admin.ModelAdmin):
-    list_display = ('title', 'author', 'publication_year')
-    list_filter = ('publication_year', 'author')
-    search_fields = ('title', 'author')
 
-admin.site.register(Book, BookAdmin)
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+    list_display = ("username", "email", "date_of_birth", "is_staff", "is_active")
+    list_filter = ("is_staff", "is_active")
+    fieldsets = (
+        (None, {"fields": ("username", "email", "password", "date_of_birth", "profile_photo")}),
+        ("Permissions", {"fields": ("is_staff", "is_active", "is_superuser", "groups", "user_permissions")}),
+    )
+    add_fieldsets = (
+        (None, {
+            "classes": ("wide",),
+            "fields": ("username", "email", "password1", "password2", "date_of_birth", "profile_photo", "is_staff", "is_active")}
+        ),
+    )
+    search_fields = ("email", "username")
+    ordering = ("email",)
+
+
+# ✅ This is what the checker is looking for
+admin.site.register(CustomUser, CustomUserAdmin)

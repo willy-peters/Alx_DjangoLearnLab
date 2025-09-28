15694063fa37385f4ba3a4acdbd8f2ef333662fb
Willy Nguyamu <nguyamuwilly@gmail.com>
2025-09-28 19:56:52 +0300

    Changes for task 2

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -1,6 +1,15 @@
-from django.urls import path
-from .views import BookList
+from django.urls import path, include
+from .views import BookList, BookViewSet
+from rest_framework.routers import DefaultRouter
+
+
+router = DefaultRouter()
+router.register(r'books_all', BookViewSet, basename='book_all')
+
 
 urlpatterns = [
     path('books/', BookList.as_view(), name='book-list'),
+
+    # Add router-generated CRUD endpoints
+    path('', include(router.urls)),
 ]

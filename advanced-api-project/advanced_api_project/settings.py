20277ad61b81ae45dcf612531292d6109d03e52f
Willy Nguyamu <nguyamuwilly@gmail.com>
2025-09-28 22:38:36 +0300

    Add filtering, searching, and ordering capabilities to BookListView

--- advanced-api-project/advanced_api_project/settings.py
+++ advanced-api-project/advanced_api_project/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'api',
+    'django_filters',
 ]
 
 MIDDLEWARE = [
@@ -122,3 +123,12 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+REST_FRAMEWORK = {
+    'DEFAULT_FILTER_BACKENDS': [
+        'django_filters.rest_framework.DjangoFilterBackend',
+        'rest_framework.filters.SearchFilter',
+        'rest_framework.filters.OrderingFilter',
+    ]
+}
--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,7 +1,8 @@
-from rest_framework import generics, permissions
+from rest_framework import generics, permissions, filters
 from .models import Book
 from .serializers import BookSerializer
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
+from django_filters.rest_framework import DjangoFilterBackend
 
 
 # ----------------------------
@@ -16,6 +17,20 @@ class BookListView(generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
     permission_classes = [permissions.AllowAny]  # Anyone can read
+
+
+    # Filtering, Searching, Ordering setup
+    filter_backends = [DjangoFilterBackend, filters.SearchFilter, filters.OrderingFilter]
+
+    # Filter by fields
+    filterset_fields = ['title', 'author__name', 'publication_year']
+
+    # Search in fields (partial matches)
+    search_fields = ['title', 'author__name']
+
+    # Allow ordering by fields
+    ordering_fields = ['title', 'publication_year']
+    ordering = ['title']  # Default ordering
 
 
 class BookDetailView(generics.RetrieveAPIView):
@@ -64,3 +79,19 @@ class BookDeleteView(generics.DestroyAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
     permission_classes = [permissions.IsAuthenticated]
+
+
+# Book API Query Features
+
+## Filtering
+"""
+- `/api/books/?publication_year=1949` → Filter by year
+- `/api/books/?author__name=George Orwell` → Filter by author
+
+## Searching
+- `/api/books/?search=Farm` → Search in title or author name
+
+## Ordering
+- `/api/books/?ordering=title` → Order alphabetically
+- `/api/books/?ordering=-publication_year` → Order by year descending
+"""

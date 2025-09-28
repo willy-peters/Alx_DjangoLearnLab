e4e2ce9d7990e891ebb6259239b4390bb199f978
Willy Nguyamu <nguyamuwilly@gmail.com>
2025-09-28 22:33:50 +0300

    Add IsAuthenticatedOrReadOnly permission to views

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,6 +1,7 @@
 from rest_framework import generics, permissions
 from .models import Book
 from .serializers import BookSerializer
+from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
 
 
 # ----------------------------

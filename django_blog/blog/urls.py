659f0b73888f911f2fbcd0e6d521a84455830f7d
Willy Nguyamu <nguyamuwilly@gmail.com>
2025-10-05 23:59:22 +0300

    Fix comment URL patterns for consistency and clarity

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -30,8 +30,8 @@ urlpatterns = [
     path('post/<int:pk>/update/', views.PostUpdateView.as_view(), name='post-update'),
     path('post/<int:pk>/delete/', views.PostDeleteView.as_view(), name='post-delete'),
 
-     # comments
-    path('post/<int:post_pk>/comments/new/', views.CommentCreateView.as_view(), name='comment-create'),
-    path('comments/<int:pk>/edit/', views.CommentUpdateView.as_view(), name='comment-update'),
-    path('comments/<int:pk>/delete/', views.CommentDeleteView.as_view(), name='comment-delete'),
+    # comments
+    path('post/<int:pk>/comments/new/', views.CommentCreateView.as_view(), name='comment-create'),
+    path('comment/<int:pk>/update/', views.CommentUpdateView.as_view(), name='comment-update'),
+    path('comment/<int:pk>/delete/', views.CommentDeleteView.as_view(), name='comment-delete'),
 ]

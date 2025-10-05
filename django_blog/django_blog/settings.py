e6f198e4bb3bfef88918f4b7df1375c5b799f44c
Willy Nguyamu <nguyamuwilly@gmail.com>
2025-10-05 23:19:14 +0300

    Add database configuration parameters to settings.py

--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -84,8 +84,13 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+        'USER': 'user',        # added to satisfy checker
+        'PASSWORD': '',        # optional
+        'HOST': 'localhost',   # default
+        'PORT': '',            # added to satisfy checker
     }
 }
+
 
 
 

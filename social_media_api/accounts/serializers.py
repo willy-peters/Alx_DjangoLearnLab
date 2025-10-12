c0c4c89d9fc0e08694ada1acc642f92f39d34a7a
Willy Nguyamu <nguyamuwilly@gmail.com>
2025-10-12 23:20:20 +0300

    Refactor user registration serializer to streamline user creation and remove unused code

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -1,13 +1,8 @@
+from django.contrib.auth import get_user_model
 from rest_framework import serializers
-from django.contrib.auth import authenticate
 from rest_framework.authtoken.models import Token
-from .models import User
 
-class UserSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = User
-        fields = ('id','username','email','first_name','last_name','bio','profile_picture')
-        read_only_fields = ('id',)
+User = get_user_model()
 
 class RegisterSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True)
@@ -15,28 +10,18 @@ class RegisterSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = User
-        fields = ('id','username','email','password','token')
+        fields = ('id', 'username', 'email', 'password', 'token')
 
     def create(self, validated_data):
-        password = validated_data.pop('password')
-        user = User(**validated_data)
-        user.set_password(password)
-        user.save()
+        # ✅ Proper way to create a user
+        user = User.objects.create_user(
+            username=validated_data['username'],
+            email=validated_data.get('email', ''),
+            password=validated_data['password']
+        )
         Token.objects.create(user=user)
         return user
 
     def get_token(self, obj):
         token = Token.objects.filter(user=obj).first()
         return token.key if token else None
-
-class LoginSerializer(serializers.Serializer):
-    username = serializers.CharField()
-    password = serializers.CharField(write_only=True)
-    token = serializers.CharField(read_only=True)
-
-    def validate(self, data):
-        user = authenticate(username=data.get('username'), password=data.get('password'))
-        if user and user.is_active:
-            token, _ = Token.objects.get_or_create(user=user)
-            return {'user': user, 'token': token.key}
-        raise serializers.ValidationError('Unable to log in with provided credentials.')
